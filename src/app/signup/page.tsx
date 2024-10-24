e2ce3590a79eb3d5ce4cb925d9f7e3caad9cfe11
Ketan Baitule <111437670+ketanbaitule@users.noreply.github.com>
2024-10-24 20:04:08 +0530

    Refractor heading of signup and login form

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -7,7 +7,7 @@ export default function SignUp() {
         <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0">
           <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
             <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl">
-              Sign up for your account
+              Sign up
             </h1>
             <form className="space-y-4 md:space-y-6" action="#">
               <div>
